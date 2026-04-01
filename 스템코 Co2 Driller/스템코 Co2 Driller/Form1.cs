f2669157908f62d52420c7d671ba5c5b891bbd3a
jwjung1219 <jwjung1219@eotechnics.com>
2026-04-01 11:02:11 +0900

    오창 스템코 Co2 Driller 초기 커밋"

---
+++ 스템코 Co2 Driller/스템코 Co2 Driller/Form1.cs
@@ -0,0 +1,15 @@
+namespace 스템코_Co2_Driller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //오창 스템코
+        }
+    }
+}
